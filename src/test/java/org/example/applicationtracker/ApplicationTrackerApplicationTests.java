d2c430c68fadbb6e65e12ecac371f25df630e971
Advaitkataria <advait.katariaak@gmail.com>
2026-08-04 18:22:47 -0300

    Fix test configuration for CI environment

--- src/test/java/org/example/applicationtracker/ApplicationTrackerApplicationTests.java
+++ src/test/java/org/example/applicationtracker/ApplicationTrackerApplicationTests.java
@@ -2,12 +2,12 @@ package org.example.applicationtracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application.properties")
 class ApplicationTrackerApplicationTests {
-
     @Test
     void contextLoads() {
     }
-
 }
